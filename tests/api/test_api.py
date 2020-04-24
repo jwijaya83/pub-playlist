70f7a4d0ea6d30a82956b85a9f68146075de1ba5
msergeyx <mihalev93@yandex.ru>
2020-04-24 19:51:30 +0300

    More tests.

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -14,14 +14,28 @@ class TestAPI:
 
     @pytest.mark.api
     def test_count_of_playlists(self, api_communicator):
-        response = api_communicator.send_query_request(schema='playlists', values=['id'], code=200)
+        response = api_communicator.send_query_request(schema='playlists', values=['id', 'name'], code=200)
         assert type(response['data']['playlists']) is list
         assert len(response['data']['playlists']) is 0
 
     @pytest.mark.api
+    def test_count_of_playlists_with_songs_parameter(self, api_communicator):
+        response = api_communicator.send_query_request(schema='playlists',
+                                                       values=['id', 'name', 'songs {\nid\nartist}'],
+                                                       code=200)
+        assert type(response['data']['playlists']) is list
+        assert len(response['data']['playlists']) is 0
+
+    @pytest.mark.api
+    def test_get_the_first_playlist(self, api_communicator):
+        response = api_communicator.send_query_request(schema='playlist(id: 0)',
+                                                       values=['id', 'name'], code=200)
+        assert response['data']['playlist'] is None
+
+    @pytest.mark.api
     def test_get_correct_random_song(self, api_communicator):
         response = api_communicator.send_query_request(
-            schema='library(id: {})'.format(randint(0, const.DEFAULT_COUNT_OF_SONGS)),
+            schema='library(id: {})'.format(randint(1, const.DEFAULT_COUNT_OF_SONGS)),
             values=['id', 'album', 'duration', 'title', 'artist'],
             code=200)
         assert response['data']['library'] is not None
