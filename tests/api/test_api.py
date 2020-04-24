a7569af085619abe2127585d16f17cd7481a4053
msergeyx <mihalev93@yandex.ru>
2020-04-24 18:07:58 +0300

    Fixed tests after server changes.

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -7,13 +7,16 @@ class TestAPI:
 
     @pytest.mark.api
     def test_count_of_songs(self, api_communicator):
-        response = api_communicator.send_query_request(schema='libraries', values=['id'], code=200)
+        response = api_communicator.send_query_request(schema='libraries',
+                                                       values=['id', 'album', 'duration', 'title', 'artist'],
+                                                       code=200)
         assert len(response['data']['libraries']) == const.DEFAULT_COUNT_OF_SONGS
 
     @pytest.mark.api
     def test_count_of_playlists(self, api_communicator):
         response = api_communicator.send_query_request(schema='playlists', values=['id'], code=200)
-        assert response['data']['playlists'] is None
+        assert type(response['data']['playlists']) is list
+        assert len(response['data']['playlists']) is 0
 
     @pytest.mark.api
     def test_get_correct_random_song(self, api_communicator):
@@ -26,19 +29,19 @@ class TestAPI:
     @pytest.mark.api
     def test_get_song_low_boundary(self, api_communicator):
         response = api_communicator.send_query_request(
-            schema='library(id: 0)', values=['id', 'album', 'duration', 'title', 'artist'], code=200)
+            schema='library(id: 1)', values=['id', 'album', 'duration', 'title', 'artist'], code=200)
         assert response['data']['library'] is not None
 
     @pytest.mark.api
     def test_get_song_out_of_low_boundary(self, api_communicator):
         response = api_communicator.send_query_request(
-            schema='library(id: -1)', values=['id', 'album', 'duration', 'title', 'artist'], code=200)
+            schema='library(id: 0)', values=['id', 'album', 'duration', 'title', 'artist'], code=200)
         assert response['data']['library'] is None
 
     @pytest.mark.api
     def test_get_song_high_boundary(self, api_communicator):
         response = api_communicator.send_query_request(
-            schema='library(id: {})'.format(const.DEFAULT_COUNT_OF_SONGS - 1),
+            schema='library(id: {})'.format(const.DEFAULT_COUNT_OF_SONGS),
             values=['id', 'album', 'duration', 'title', 'artist'],
             code=200)
         assert response['data']['library'] is not None
@@ -46,7 +49,7 @@ class TestAPI:
     @pytest.mark.api
     def test_get_song_out_of_high_boundary(self, api_communicator):
         response = api_communicator.send_query_request(
-            schema='library(id: {})'.format(const.DEFAULT_COUNT_OF_SONGS),
+            schema='library(id: {})'.format(const.DEFAULT_COUNT_OF_SONGS + 1),
             values=['id', 'album', 'duration', 'title', 'artist'],
             code=200)
         assert response['data']['library'] is None
