6947c46873b3a61e9dc72000081adda3ecf174d1
msergeyx <mihalev93@yandex.ru>
2020-04-28 14:55:16 +0300

    Tests for createPlaylist ready.

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,6 +1,7 @@
 import pytest
 from random import randint
 import tests.helpers.data_generator as dg
+from time import sleep
 
 
 class TestAPI:
@@ -10,31 +11,49 @@ class TestAPI:
         response = api_communicator.send_query_request(schema='libraries',
                                                        values=['id', 'album', 'duration', 'title', 'artist'],
                                                        code=200)
-        assert len(response['data']['libraries']) == expectations.get_count_of_songs()
+        assert len(response['data']['libraries']) == expectations.get_songs_count()
 
     @pytest.mark.api
-    def test_count_of_playlists(self, api_communicator):
+    def test_count_of_playlists(self, api_communicator, expectations):
         response = api_communicator.send_query_request(schema='playlists', values=['id', 'name'], code=200)
         assert type(response['data']['playlists']) is list
-        assert len(response['data']['playlists']) is 0
+        assert len(response['data']['playlists']) == expectations.get_playlists_count()
 
     @pytest.mark.api
-    def test_count_of_playlists_with_songs_parameter(self, api_communicator):
+    def test_count_of_playlists_with_songs_parameter(self, api_communicator, expectations):
         response = api_communicator.send_query_request(schema='playlists',
                                                        values=['id', 'name', 'songs {\nid\nartist}'],
                                                        code=200)
         assert type(response['data']['playlists']) is list
-        assert len(response['data']['playlists']) is 0
+        assert len(response['data']['playlists']) == expectations.get_playlists_count()
 
     @pytest.mark.api
-    def test_get_the_first_playlist(self, api_communicator):
+    def test_check_playlists_low_boundary(self, api_communicator):
+        response = api_communicator.send_query_request(schema='playlist(id: 1)',
+                                                       values=['id', 'name'], code=200)
+        assert response['data']['playlist'] is not None
+
+    @pytest.mark.api
+    def test_check_playlists_out_of_low_boundary(self, api_communicator):
         response = api_communicator.send_query_request(schema='playlist(id: 0)',
                                                        values=['id', 'name'], code=200)
         assert response['data']['playlist'] is None
 
     @pytest.mark.api
+    def test_check_playlists_high_boundary(self, api_communicator, expectations):
+        response = api_communicator.send_query_request(schema='playlist(id: {})'.format(expectations.get_playlists_count()),
+                                                       values=['id', 'name'], code=200)
+        assert response['data']['playlist'] is not None
+
+    @pytest.mark.api
+    def test_check_playlists_out_of_low_boundary(self, api_communicator, expectations):
+        response = api_communicator.send_query_request(schema='playlist(id: {})'.format(expectations.get_playlists_count() + 1),
+                                                       values=['id', 'name'], code=200)
+        assert response['data']['playlist'] is None
+
+    @pytest.mark.api
     def test_get_correct_random_song(self, api_communicator, expectations):
-        song_id = randint(1, expectations.get_count_of_songs())
+        song_id = randint(1, expectations.get_songs_count())
         response = api_communicator.send_query_request(
             schema='library(id: {})'.format(song_id),
             values=['id', 'album', 'duration', 'title', 'artist'],
@@ -58,16 +77,16 @@ class TestAPI:
     @pytest.mark.api
     def test_get_song_high_boundary(self, api_communicator, expectations):
         response = api_communicator.send_query_request(
-            schema='library(id: {})'.format(expectations.get_count_of_songs()),
+            schema='library(id: {})'.format(expectations.get_songs_count()),
             values=['id', 'album', 'duration', 'title', 'artist'],
             code=200)
         assert response['data']['library'] is not None
-        assert response['data']['library'] == expectations.get_song_by_id(expectations.get_count_of_songs()).to_dict()
+        assert response['data']['library'] == expectations.get_song_by_id(expectations.get_songs_count()).to_dict()
 
     @pytest.mark.api
     def test_get_song_out_of_high_boundary(self, api_communicator, expectations):
         response = api_communicator.send_query_request(
-            schema='library(id: {})'.format(expectations.get_count_of_songs() + 1),
+            schema='library(id: {})'.format(expectations.get_songs_count() + 1),
             values=['id', 'album', 'duration', 'title', 'artist'],
             code=200)
         assert response['data']['library'] is None
@@ -80,7 +99,7 @@ class TestAPI:
             func='createPlaylist(name: "{}")'.format(dg.generate_random_name(8)),
             values=['id'],
             code=200)
-        assert response['data']['createPlaylist'] is not None
+        assert response['data']['createPlaylist'] is not None, print(response['data'])
         expectations.update_playlists_df()
         assert count_of_playlists + 1 == expectations.get_playlists_count()
 
@@ -92,6 +111,19 @@ class TestAPI:
             func='createPlaylist(name: "")',
             values=['id'],
             code=400)
+        assert response['data']['createPlaylist'] is None
+        expectations.update_playlists_df()
+        assert count_of_playlists == expectations.get_playlists_count()
+
+    @pytest.mark.api
+    def test_create_empty_playlist_with_existing_name(self, api_communicator, expectations):
+        expectations.update_playlists_df()
+        count_of_playlists = expectations.get_playlists_count()
+        response = api_communicator.send_mutation_request(
+            func='createPlaylist(name: "{}")'.format(expectations.get_playlist_name_by_id(randint(1, count_of_playlists))),
+            values=['id'],
+            code=400)
+        assert response['data']['createPlaylist'] is None
         expectations.update_playlists_df()
         assert count_of_playlists == expectations.get_playlists_count()
 
@@ -134,10 +166,32 @@ class TestAPI:
     def test_create_playlist_with_incorrect_songs(self, api_communicator, expectations):
         expectations.update_playlists_df()
         count_of_playlists = expectations.get_playlists_count()
+        count_of_songs_in_playlists = expectations.get_count_of_songs_in_playlists()
 
         response = api_communicator.send_mutation_request(
             func='createPlaylist(name: "{}", songs: [{}])'.format(dg.generate_random_name(8),
-                                                                  expectations.get_count_of_songs() + 10),
+                                                                  expectations.get_songs_count() + 1),
             values=['id'],
-            code=400)
+            code=200)
         assert response['data']['createPlaylist'] is not None
+        expectations.update_playlists_df()
+        assert count_of_playlists + 1 == expectations.get_playlists_count()
+        assert count_of_songs_in_playlists == expectations.get_count_of_songs_in_playlists()
+
+    @pytest.mark.api
+    def test_create_playlist_with_all_songs(self, api_communicator, expectations):
+        expectations.update_playlists_df()
+        count_of_playlists = expectations.get_playlists_count()
+        count_of_songs_in_playlists = expectations.get_count_of_songs_in_playlists()
+        count_of_songs = expectations.get_songs_count()
+
+        response = api_communicator.send_mutation_request(
+            func='createPlaylist(name: "{}", songs: {})'.format(dg.generate_random_name(8),
+                                                                str([x for x in range(1, count_of_songs + 1)])),
+            values=['id'],
+            code=200)
+        assert response['data']['createPlaylist'] is not None
+        sleep(0.2)
+        expectations.update_playlists_df()
+        assert count_of_playlists + 1 == expectations.get_playlists_count()
+        assert count_of_songs_in_playlists + count_of_songs == expectations.get_count_of_songs_in_playlists()
--- tests/expectations.py
+++ tests/expectations.py
@@ -9,9 +9,9 @@ class PlaylistExpectations:
         self.songs_df['name'] = self.songs_df['id']
         self.songs_df.set_index('name', inplace=True)
         self.playlists_df = pd.DataFrame(data=playlists, columns=playlists_col_names)
-        self.songs_in_playlists = pd.DataFrame(data=songs_in_playlists, columns=sip_names)
+        self.songs_in_playlists_table_df = pd.DataFrame(data=songs_in_playlists, columns=sip_names)
 
-    def get_count_of_songs(self):
+    def get_songs_count(self):
         return self.songs_df.iloc[-1].name
 
     def get_song_by_id(self, id):
@@ -20,10 +20,16 @@ class PlaylistExpectations:
     def get_playlists_count(self):
         return self.playlists_df['id'].count()
 
+    def get_playlist_name_by_id(self, id):
+        return self.playlists_df[self.playlists_df['id'] == id]['name']
+
+    def get_count_of_songs_in_playlists(self):
+        return self.songs_in_playlists_table_df['song_id'].count()
+
     def update_playlists_df(self):
         db_communicator = DBCommunicator()
         self.playlists_df = pd.DataFrame(data=db_communicator.get_playlists_list(),
                                          columns=db_communicator.get_playlists_table_columns_names())
-        self.songs_in_playlists = pd.DataFrame(data=db_communicator.get_all_songs_in_playlists(),
-                                               columns=db_communicator.get_all_songs_in_playlists_columns_names())
+        self.songs_in_playlists_table_df = pd.DataFrame(data=db_communicator.get_all_songs_in_playlists(),
+                                                        columns=db_communicator.get_all_songs_in_playlists_columns_names())
         db_communicator.__del__()
