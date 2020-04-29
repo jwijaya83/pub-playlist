811f8e4f923e973763a8d2a51b95e588a1249298
Dmitry Osinkin <dm.osinkin@gmail.com>
2020-04-29 16:26:47 +0300

    Added playlist creating action

--- ui/components/Playlists/Playlists.jsx
+++ ui/components/Playlists/Playlists.jsx
@@ -7,44 +7,71 @@ import {Query} from "react-apollo";
 import gql from "graphql-tag";
 import {Playlist} from "../Playlist/Playlist";
 
-export class Playlists extends Component {
-    render() {
-
-        return <Query query={gql`
-            query {
-                playlists {
-                    id,
-                    name,
-                    songs {
-                        id,
-                        album,
-                        duration,
-                        title,
-                        artist
-                    }
-                }
+const GET_PLAYLISTS = gql`
+    query {
+        playlists {
+            id,
+            name,
+            songs {
+                id,
+                album,
+                duration,
+                title,
+                artist
             }
-        `}
-        >
-            {({loading, error, data}) => {
-                if (loading) return <NoResults message="Loading..."/>;
-                if (error) return <p>Error :(</p>;
-                if (data.playlists.count === 0) return <NoResults
-                    message="Fill free to create your personal playlists"/>
+        }
+    }
+`
 
-                return (
-                    <div className="playlistList">
-                        <TopBar title="My Playlists">
-                            <NewPlaylist/>
-                        </TopBar>
-                        <div className="playlists">
-                            {data.playlists.map((playlist, index) => (
-                                <div key={index}><Playlist playlist={playlist} index={index}/></div>
-                            ))}
-                        </div>
-                    </div>
-                );
-            }}
+export class Playlists extends Component {
+    constructor(props) {
+        super(props);
+        this.handlePlaylistsUpdating = this.handlePlaylistsUpdating.bind(this);
+        this.isNewPlaylistCreated = false
+        this.playlists = Array(0)
+        this.state = {
+            trigger: false
+        };
+    }
+
+    handlePlaylistsUpdating(playlist) {
+        this.isNewPlaylistCreated = true
+        this.playlists.push(playlist);
+        this.forceUpdate()
+    }
+
+    componentDidMount() {
+        if (this.isNewPlaylistCreated) {
+            this.isNewPlaylistCreated = false
+        }
+    }
+
+    createPlaylists(playlists) {
+        return (<div className="playlistList">
+            <TopBar title="My Playlists">
+                <NewPlaylist handlePlaylistsUpdating={this.handlePlaylistsUpdating}/>
+            </TopBar>
+            <div className="playlists">
+                {playlists.map((playlist, index) => (
+                    <div key={index}><Playlist playlist={playlist} index={index}/></div>
+                ))}
+            </div>
+        </div>);
+    }
+
+    render() {
+        return <Query query={GET_PLAYLISTS}>{({loading, error, data}) => {
+            if (loading) return <NoResults message="Loading..."/>;
+            if (error) return <p>Error :(</p>;
+            if (data.playlists.count === 0) return <NoResults
+                message="Fill free to create your personal playlists"/>;
+
+            if (!this.isNewPlaylistCreated) {
+                this.playlists = Array(0).concat(data.playlists)
+            }
+
+            return this.createPlaylists(this.playlists);
+        }}
         </Query>;
     }
 }
